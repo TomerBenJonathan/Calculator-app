29e58e60c5f89b1e905908cfcb1ff6c58b210d4d
TomerBenJonathan <117282271+TomerBenJonathan@users.noreply.github.com>
2022-11-19 17:58:00 +0200

    changes index.html

--- js/calculator.js
+++ js/calculator.js
@@ -1,13 +1,22 @@
 
-let cur_value =null;
-let first_operand= null;
-let cur_operator = null;
-let second_operand = null;
+let cur_value ="";
+let first_operand= "";
+let cur_operator = "";
+let second_operand = "";
 
 function insert(num) {
     document.form1.textview.value = document.form1.textview.value + num;
     
+
+     
+    
 }
+
+ // if (cur_operator===""){
+    // first_operand=first_operand + num;}
+    // else if(cur_operator!==""){
+    // second_operand =  second_operand + num;} 
+    // document.form1.textview.value= first_operand+ cur_operator+ second_operand;  
 
 function insertoperator(num) {
     if(document.form1.textview.value.slice(-1)==='*'||document.form1.textview.value.slice(-1)==='/'|| document.form1.textview.value.slice(-1)==='+'||document.form1.textview.value.slice(-1)==='-'){
@@ -20,7 +29,20 @@ function insertoperator(num) {
         document.form1.textview.value = document.form1.textview.value + num;
     }
 }
+         
 }
+
+ // if(second_operand !==""){
+  // const exp = document.form1.textview.value;
+  // if (exp) {
+  //      =eval(exp)
+  //      document.form1.textview.value =first_operand+ num;
+  //   cur_operator=num;}
+  // else{ 
+  // cur_operator=num;
+  // document.form1.textview.value= first_operand + cur_operator;
+
+
 function alertNum(el){
     alert(el);
 }
@@ -31,18 +53,37 @@ function equal() {
         document.form1.textview.value = eval(exp);
     }
     alertNum(eval(exp))
+    /// add >> first_operand= eval(exp) ;
+    ///              cur_value =eval(exp) ;
+    /// cur_operator = "";
+    /// second_operand = "";
+
 }
+
+//
 
 function backspace() {
     const exp = document.form1.textview.value;
     document.form1.textview.value = exp.substring(0, exp.length - 1);
 }
 
+ // if(second_operand !==""){
+    //const exp = second_operand;
+    //second_operand= exp.substring(0, exp.length - 1);
+   //}else if(cur_opraor!==""){
+    //cur_opraor="";
+    //}else if(first_operand!==""){
+    //const exp = first_operand;
+    //first_operand= exp.substring(0, exp.length - 1);
+    //}
+    // first_operand + first_operator + second_operand;
+
 function clear(){
-    let cur_value ;
-    let first_operand;
-    let first_operator ;
-    let second_operand ;
+    //cur_value="" ;
+   // first_operand="";
+   // first_operator="" ;
+   // second_operand="" ;
+   // document.form1.textview.value= first_operand + first_operator + second_operand;
 
 }
 
@@ -64,8 +105,20 @@ function myFunction() {
 
 }
 
-function classToggle() {
-    this.classList.toggle('ccalculator-light');
-    this.classList.toggle('calculator-dark');
-}
-document.querySelector('#cury').addEventListener('click', classToggle);
+function mylight(){
+    var divEle = document.getElementById("cury"),
+        current_class = divEle.className;
+    divEle.className = (current_class == "calculator-light") ? "calculator-dark" : "calculator-light";
+  }
+
+
+  function changeColor() { 
+    var userColor = document.getElementById('colorID').value;
+    localStorage.setItem('storedValue', document.body.style.backgroundColor = userColor);
+  }
+  
+  // if there is a value stored, update color picker and background color
+  if(localStorage.storedValue) {
+    document.getElementById('colorID').value = localStorage.storedValue;
+    .body.style.backgroundColor      = localStorage.storedValue;
+  }
--- js/dist/calculator.dev.js
+++ js/dist/calculator.dev.js
@@ -1,13 +1,18 @@
 "use strict";
 
-var cur_value = null;
-var first_operand = null;
-var cur_operator = null;
-var second_operand = null;
+var cur_value = "";
+var first_operand = "";
+var cur_operator = "";
+var second_operand = "";
 
 function insert(num) {
   document.form1.textview.value = document.form1.textview.value + num;
-}
+} // if (cur_operator===""){
+// first_operand=first_operand + num;}
+// else if(cur_operator!==""){
+// second_operand =  second_operand + num;} 
+// document.form1.textview.value= first_operand+ cur_operator+ second_operand;  
+
 
 function insertoperator(num) {
   if (document.form1.textview.value.slice(-1) === '*' || document.form1.textview.value.slice(-1) === '/' || document.form1.textview.value.slice(-1) === '+' || document.form1.textview.value.slice(-1) === '-') {
@@ -20,7 +25,16 @@ function insertoperator(num) {
       document.form1.textview.value = document.form1.textview.value + num;
     }
   }
-}
+} // if(second_operand !==""){
+// const exp = document.form1.textview.value;
+// if (exp) {
+//      =eval(exp)
+//      document.form1.textview.value =first_operand+ num;
+//   cur_operator=num;}
+// else{ 
+// cur_operator=num;
+// document.form1.textview.value= first_operand + cur_operator;
+
 
 function alertNum(el) {
   alert(el);
@@ -33,19 +47,33 @@ function equal() {
     document.form1.textview.value = eval(exp);
   }
 
-  alertNum(eval(exp));
-}
+  alertNum(eval(exp)); /// add >> first_operand= eval(exp) ;
+  ///              cur_value =eval(exp) ;
+  /// cur_operator = "";
+  /// second_operand = "";
+} //
+
 
 function backspace() {
   var exp = document.form1.textview.value;
   document.form1.textview.value = exp.substring(0, exp.length - 1);
-}
+} // if(second_operand !==""){
+//const exp = second_operand;
+//second_operand= exp.substring(0, exp.length - 1);
+//}else if(cur_opraor!==""){
+//cur_opraor="";
+//}else if(first_operand!==""){
+//const exp = first_operand;
+//first_operand= exp.substring(0, exp.length - 1);
+//}
+// first_operand + first_operator + second_operand;
 
-function clear() {
-  var cur_value;
-  var first_operand;
-  var first_operator;
-  var second_operand;
+
+function clear() {//cur_value="" ;
+  // first_operand="";
+  // first_operator="" ;
+  // second_operand="" ;
+  // document.form1.textview.value= first_operand + first_operator + second_operand;
 }
 
 function power() {
@@ -66,9 +94,19 @@ function myFunction() {
   backspace();
 }
 
-function classToggle() {
-  this.classList.toggle('ccalculator-light');
-  this.classList.toggle('calculator-dark');
+function mylight() {
+  var divEle = document.getElementById("cury"),
+      current_class = divEle.className;
+  divEle.className = current_class == "calculator-light" ? "calculator-dark" : "calculator-light";
 }
 
-document.querySelector('#cury').addEventListener('click', classToggle);
+function changeColor() {
+  var userColor = document.getElementById('colorID').value;
+  localStorage.setItem('storedValue', document.body.style.backgroundColor = userColor);
+} // if there is a value stored, update color picker and background color
+
+
+if (localStorage.storedValue) {
+  document.getElementById('colorID').value = localStorage.storedValue;
+  document.body.style.backgroundColor = localStorage.storedValue;
+}
