c829923aa130f147b295315587336eab9f652d7b
TomerBenJonathan <117282271+TomerBenJonathan@users.noreply.github.com>
2022-11-19 21:11:50 +0200

    app responsiveness

--- js/calculator.js
+++ js/calculator.js
@@ -1,49 +1,49 @@
+import {
+    setItem,
+    storedValue
+} from 'local-storage';
 
-let cur_value ="";
-let first_operand= "";
-let cur_operator = "";
-let second_operand = "";
+// let cur_value ="";
+// let first_operand= "";
+// let cur_operator = "";
+// let second_operand = "";
 
 function insert(num) {
     document.form1.textview.value = document.form1.textview.value + num;
-    
-
-     
-    
 }
 
- // if (cur_operator===""){
-    // first_operand=first_operand + num;}
-    // else if(cur_operator!==""){
-    // second_operand =  second_operand + num;} 
-    // document.form1.textview.value= first_operand+ cur_operator+ second_operand;  
+// if (cur_operator===""){
+// first_operand=first_operand + num;}
+// else if(cur_operator!==""){
+// second_operand =  second_operand + num;} 
+// document.form1.textview.value= first_operand+ cur_operator+ second_operand;  
 
 function insertoperator(num) {
-    if(document.form1.textview.value.slice(-1)==='*'||document.form1.textview.value.slice(-1)==='/'|| document.form1.textview.value.slice(-1)==='+'||document.form1.textview.value.slice(-1)==='-'){
-        document.form1.textview.value=document.form1.textview.value.slice(0,-1)+num;
-    }else{
-    
-    const exp = document.form1.textview.value;
-    if (exp) {
-        document.form1.textview.value = eval(exp);
-        document.form1.textview.value = document.form1.textview.value + num;
+    if (document.form1.textview.value.slice(-1) === '*' || document.form1.textview.value.slice(-1) === '/' || document.form1.textview.value.slice(-1) === '+' || document.form1.textview.value.slice(-1) === '-') {
+        document.form1.textview.value = document.form1.textview.value.slice(0, -1) + num;
+    } else {
+
+        const exp = document.form1.textview.value;
+        if (exp) {
+            document.form1.textview.value = eval(exp);
+            document.form1.textview.value = document.form1.textview.value + num;
+        }
     }
+
 }
-         
-}
 
- // if(second_operand !==""){
-  // const exp = document.form1.textview.value;
-  // if (exp) {
-  //      =eval(exp)
-  //      document.form1.textview.value =first_operand+ num;
-  //   cur_operator=num;}
-  // else{ 
-  // cur_operator=num;
-  // document.form1.textview.value= first_operand + cur_operator;
+// if(second_operand !==""){
+// const exp = document.form1.textview.value;
+// if (exp) {
+//      =eval(exp)
+//      document.form1.textview.value =first_operand+ num;
+//   cur_operator=num;}
+// else{ 
+// cur_operator=num;
+// document.form1.textview.value= first_operand + cur_operator;
 
 
-function alertNum(el){
+function alertNum(el) {
     alert(el);
 }
 
@@ -67,34 +67,34 @@ function backspace() {
     document.form1.textview.value = exp.substring(0, exp.length - 1);
 }
 
- // if(second_operand !==""){
-    //const exp = second_operand;
-    //second_operand= exp.substring(0, exp.length - 1);
-   //}else if(cur_opraor!==""){
-    //cur_opraor="";
-    //}else if(first_operand!==""){
-    //const exp = first_operand;
-    //first_operand= exp.substring(0, exp.length - 1);
-    //}
-    // first_operand + first_operator + second_operand;
+// if(second_operand !==""){
+//const exp = second_operand;
+//second_operand= exp.substring(0, exp.length - 1);
+//}else if(cur_opraor!==""){
+//cur_opraor="";
+//}else if(first_operand!==""){
+//const exp = first_operand;
+//first_operand= exp.substring(0, exp.length - 1);
+//}
+// first_operand + first_operator + second_operand;
 
-function clear(){
+function clear() {
     //cur_value="" ;
-   // first_operand="";
-   // first_operator="" ;
-   // second_operand="" ;
-   // document.form1.textview.value= first_operand + first_operator + second_operand;
+    // first_operand="";
+    // first_operator="" ;
+    // second_operand="" ;
+    // document.form1.textview.value= first_operand + first_operator + second_operand;
 
 }
 
-function power(){
+function power() {
     const exp = document.form1.textview.value;
     if (exp) {
-        document.form1.textview.value = eval(exp)**2;
-}
+        document.form1.textview.value = eval(exp) ** 2;
+    }
 }
 
-function promptAlert(){
+function promptAlert() {
     alert("Developer: Tomer Ben Jonathan" + "\n" + "Version: 0.1" + "\n" + "Description: Calculator App");
 }
 
@@ -105,20 +105,9 @@ function myFunction() {
 
 }
 
-function mylight(){
+function mylight() {
     var divEle = document.getElementById("cury"),
         current_class = divEle.className;
     divEle.className = (current_class == "calculator-light") ? "calculator-dark" : "calculator-light";
-  }
+}
 
-
-  function changeColor() { 
-    var userColor = document.getElementById('colorID').value;
-    localStorage.setItem('storedValue', document.body.style.backgroundColor = userColor);
-  }
-  
-  // if there is a value stored, update color picker and background color
-  if(localStorage.storedValue) {
-    document.getElementById('colorID').value = localStorage.storedValue;
-    .body.style.backgroundColor      = localStorage.storedValue;
-  }
--- js/dist/calculator.dev.js
+++ js/dist/calculator.dev.js
@@ -1,10 +1,11 @@
 "use strict";
 
-var cur_value = "";
-var first_operand = "";
-var cur_operator = "";
-var second_operand = "";
+var _localStorage = require("local-storage");
 
+// let cur_value ="";
+// let first_operand= "";
+// let cur_operator = "";
+// let second_operand = "";
 function insert(num) {
   document.form1.textview.value = document.form1.textview.value + num;
 } // if (cur_operator===""){
@@ -98,15 +99,4 @@ function mylight() {
   var divEle = document.getElementById("cury"),
       current_class = divEle.className;
   divEle.className = current_class == "calculator-light" ? "calculator-dark" : "calculator-light";
-}
-
-function changeColor() {
-  var userColor = document.getElementById('colorID').value;
-  localStorage.setItem('storedValue', document.body.style.backgroundColor = userColor);
-} // if there is a value stored, update color picker and background color
-
-
-if (localStorage.storedValue) {
-  document.getElementById('colorID').value = localStorage.storedValue;
-  document.body.style.backgroundColor = localStorage.storedValue;
 }
